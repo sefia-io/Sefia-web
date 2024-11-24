e6ed1c08848659b6245189b30465790c9abb2b95
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-11-24 20:34:29 +0900

    Add Loading Component

--- src/components/Guard/InitializationGuard.tsx
+++ src/components/Guard/InitializationGuard.tsx
@@ -1,28 +1,37 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { ReactNode, useEffect } from "react";
 import { useServerStore } from "@/store/serverStore";
+import Loading from "../Loading";
 
 const InitializationGuard = ({ children }: { children: ReactNode }) => {
-    const { isInitialized, checkInitialization } = useServerStore();
-    const location = useLocation();
+  const { isInitialized, checkInitialization } = useServerStore();
+  const location = useLocation();
 
-    useEffect(() => {
-        checkInitialization();
-    }, [checkInitialization]);
+  useEffect(() => {
+    checkInitialization();
+  }, [checkInitialization]);
 
-    if (!isInitialized) {
-        return location.pathname === "/get-started" ? (
-            <>{children}</>
-        ) : (
-            <Navigate to="/get-started" />
-        );
-    }
+  if (isInitialized === undefined) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loading />
+      </div>
+    );
+  }
 
-    if (isInitialized && location.pathname === "/get-started") {
-        return <Navigate to="/" />;
-    }
+  if (!isInitialized) {
+    return location.pathname === "/get-started" ? (
+      <>{children}</>
+    ) : (
+      <Navigate to="/get-started" />
+    );
+  }
 
-    return <>{children}</>;
+  if (isInitialized && location.pathname === "/get-started") {
+    return <Navigate to="/" />;
+  }
+
+  return <>{children}</>;
 };
 
 export default InitializationGuard;
