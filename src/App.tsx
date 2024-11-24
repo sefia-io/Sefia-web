bd9f70dd55b85fa5f0c66457dd16f7e167c30dfe
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-11-24 20:35:06 +0900

    apply ServerStatusGuard

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/Home"
 import LoginPage from "./pages/SignIn"
 import InitializationGuard from "./components/Guard/InitializationGuard"
+import GetStarted from "./pages/GetStarted"
+import ServerStatusGuard from "./components/Guard/ServerStatusGuard"
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
-        <InitializationGuard>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/signin" element={<LoginPage />} />
-          </Routes>
-        </InitializationGuard>
+        <ServerStatusGuard>
+          <InitializationGuard>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/get-started" element={<GetStarted />} />
+              <Route path="/signin" element={<LoginPage />} />
+            </Routes>
+          </InitializationGuard>
+        </ServerStatusGuard>
       </BrowserRouter>
     </ThemeProvider>
   )
